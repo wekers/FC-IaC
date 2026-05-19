78ca1bf2af495a86ce091288f4e0a3656d2d49e5
Fernando Gilli <fernando@wekers.org>
2026-05-19 12:16:20 -0300

    feat: add separate Dev and Prod website stacks with account and region specifications

--- CDK/website/app.py
+++ CDK/website/app.py
@@ -6,6 +6,20 @@ import aws_cdk as cdk
 from website.website_stack import WebsiteStack
 
 app = cdk.App()
-WebsiteStack(app, "WebsiteStack", "website-")
+
+# Example Dev and Prod stacks are in different accounts, so we need to specify the account and region for each stack
+WebsiteStack(
+    app,
+    "DevWebsiteStack",
+    "dev-website-",
+    env=cdk.Environment(account="304851244121", region="us-west-2"),
+)
+WebsiteStack(
+    app,
+    "ProdWebsiteStack",
+    "prod-website-",
+    env=cdk.Environment(account="590183902691", region="us-west-2"),
+)
+
 
 app.synth()
